84ef2d8713f574843c8e248fa625b95c0b7e9591
Alfi Alhuzwiri Syam <alhuzwirialfi@gmail.com>
2026-01-29 00:44:20 +0700

    update fitur

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true,  // Agar bisa diakses dari luar container
+    port: 5173,
+    
+    // --- TAMBAHKAN BAGIAN INI ---
+    allowedHosts: [
+      'gentongai.inventorycafe.space', 
+      'inventorycafe.space',
+      'all' // Opsi malas: izinkan semua (opsional)
+    ],
+    // ----------------------------
+  }
 })
-
-
-
